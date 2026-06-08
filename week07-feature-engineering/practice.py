0deaa113cedb8752d00937a1356bc9e4d2eddc50
shefali-walia <shefali.walia.04@gmail.com>
2026-06-08 12:36:29 +0530

    Week07-feature-engineering: practice.py - Task1 Log transformation and comparision of PM2.5

--- week07-feature-engineering/practice.py
+++ week07-feature-engineering/practice.py
@@ -32,3 +32,57 @@ print("Data types: \n",df_use.dtypes)
 # Since all variables are numeric, we don't need to convert
 
 #-------------------------------------------------
+# LOG TRANSFORMATION ON PM2.5
+# From Week4 histogram - PM2.5 is right-skewed (long tail towards right)
+# Plot before and after histograms side by side and train log. reg. on both and compare F1 score
+
+plt.figure(figsize = (8,6))
+# Before chart
+plt.subplot(1,2,1)
+plt.hist(df_use['PM2.5'], bins = 30, edgecolor = 'black', range = (0,800))
+plt.title("Without Log Transformation", fontsize = 10)
+plt.xlabel("PM2.5")
+plt.ylabel("Frequency")
+plt.grid(True, linestyle = '--', alpha = 0.5)
+
+df_use['PM2.5_log'] = np.log1p(df_use['PM2.5'])
+
+# After chart
+plt.subplot(1,2,2)
+plt.hist(df_use['PM2.5_log'], bins = 30, edgecolor = 'black') 
+plt.title("With Log Transformation", fontsize = 10)
+plt.xlabel("log(1 + PM2.5)")
+plt.grid(True, linestyle = '--', alpha = 0.5)
+
+plt.show()
+# Interpretation: skewness reduces, much more symmetric distribution after log transformation, compressed between range 2-6
+
+# Training log. reg. models on both raw data and transformed data and comparing f1 scores
+
+df_use['AQI_flg'] = df_use['AQI'].map(lambda x: 1 if x > 100 else 0)  # Binary target 
+
+X_raw = df_use[['PM2.5']]
+X_log = df_use[['PM2.5_log']]    # double brackets to pass X as Dataframe instead of series
+y = df_use['AQI_flg']
+
+X_raw_train, X_raw_test, y_train, y_test = train_test_split(X_raw, y, test_size=0.2, random_state=42)
+X_log_train, X_log_test, _, _ = train_test_split(X_log, y, test_size=0.2, random_state=42)  # y is already split, dont need now, so discard those values by using _,_
+
+model = LogisticRegression()
+model.fit(X_raw_train, y_train)
+y_pred = model.predict(X_raw_test)
+f1_raw = f1_score(y_test, y_pred)
+print(f"F1 score for raw: {f1_raw:.3f}")
+
+model.fit(X_log_train, y_train)
+y_pred = model.predict(X_log_test)
+f1_log = f1_score(y_test, y_pred)
+print(f"F1 score for log: {f1_log:.3f}")
+
+# Result: 
+# F1 score for raw = 0.862, for log = 0.857 = lower
+# This is expected, Logistic Regression with a single feature on a large dataset already separates classes reasonably well
+# Log transform helps more when extreme outliers are distorting a linear decision boundary across multiple features
+# In a Smart City deployment, log-transforming PM2.5 still makes sense for model stability and interpretability, even if F1 gain is minimal here
+
+#-------------------------------------------------
