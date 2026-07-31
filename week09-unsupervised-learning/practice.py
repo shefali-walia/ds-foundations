ab3d94c50ae3facb630791018eb5eff2e1ee47fa
shefali-walia <shefali.walia.04@gmail.com>
2026-07-31 18:11:33 +0530

    Week09-unsupervised-learning: practice.py - Kmeans, elbow method, Clustering and binning

--- week09-unsupervised-learning/practice.py
+++ week09-unsupervised-learning/practice.py
@@ -37,3 +37,43 @@ print("Data types: \n",df_use.dtypes)
 # Since all variables are numeric, we don't need to convert
 
 #-------------------------------------------------
+# K-MEANS + ELBOW METHOD
+
+# scale first becuase features need to be on the same scale because kmeans measures distance
+sc = StandardScaler()
+X_scaled = sc.fit_transform(df_use[['PM2.5', 'NO2', 'PM10']])
+
+# create and fit the model
+inertia_list  = []
+for i in range(1,11):
+    kmeans = KMeans(n_clusters=i, random_state= 42)
+    kmeans.fit(X_scaled)  # train the model
+    inertia_list.append(kmeans.inertia_)  # inertia_ attribute holds the "tightness" score
+
+plt.plot(range(1,11), inertia_list, marker= '+')
+plt.xlabel('No. of clusters')
+plt.ylabel('Distortion')
+plt.show()
+# result: elbow comes around k=3 or 4, depends on judgement- wherever the curve stops falling steeply and starts flattening into a long tail
+# can take 3 or 4 both 
+
+# FIT + MERGE 
+kmeans = KMeans(n_clusters=4, random_state= 42)
+kmeans.fit(X_scaled)
+
+labels = pd.Series(kmeans.labels_, name='Cluster Number')
+print(labels.value_counts(sort=False))
+
+df_use_with_clusters = pd.concat([df_use, labels], axis=1)
+print(df_use_with_clusters.head())
+
+#-------------------------------------------------
+# CROSS-TAB CLUSTERS VS SEVERITY
+bins = [0,50,100,200,800]
+bin_labels = ['Good', 'Moderate', 'Unhealthy', 'Hazardous']
+df_use_with_clusters['aqi_bin'] = pd.cut(df_use_with_clusters['AQI'], bins = bins, labels = bin_labels)
+print(df_use_with_clusters.head())
+print(df_use_with_clusters.groupby(['Cluster Number', 'aqi_bin']).size().unstack())
+# result: Cluster 1 = most unhealthy - hazardous days
+
+#-------------------------------------------------
