7196db89b36fd87ad0c18376bbe7d70b58bea459
shefali-walia <shefali.walia.04@gmail.com>
2026-08-11 15:34:56 +0530

    Week10-time-series: notes.py: Predicting and observing time series data: Autocorrelation, STL decomposition

--- week10-time-series/notes.py
+++ week10-time-series/notes.py
@@ -121,3 +121,115 @@ df_dates['day_sin'] = df_dates['day'].apply(lambda x: np.sin(2*np.pi * x/31))
 print(df_dates)
 
 #-------------------------------------------------
+# PREDICTING TIME SERIES DATA
+# we create a model that takes n time series values immediately preceding the target time as input and outputs the predicted value at the target time
+
+# Mainly 2 approaches: statistical and ML
+
+# Statistical: predictions are made based on the assumption that time series data has specific statistical properties
+# Since the model parameters and their relationship with the data are relatively interpretable, it is easier to understand how the predictions are made.
+# but if assumptions of stat. properties wrong, then prediction performance decreases
+# ARIMA, SSM
+
+# ML: can capture nonlinear relationships and complex patterns while making predictions.
+# no need to assume statistical properties in the data
+# more complex, making it difficult to understand how predictions are made         
+# But can be made better with more training data
+# Non-time-series methods like Random forest, SVM etc, time series methods like RNN, transformer etc.
+
+# autocorrelation coefficient is calculated as the correlation between the current data and data from k periods ago (lagk)
+# values from -1 to 1
+# lag = shift in time
+# At lag 0, the correlation is with itself, so the autocorrelation is always 1
+# confidence interval: if the bars fall within this band, we can say there is no significant autocorrelation at that lag. Bars extending outside the band indicate the presence of significant autocorrelation.
+# Correlogram plots the autocorrelation coefficients with lag as the x-axis.
+
+#-------------------------------------------------
+# OBSERVING TIME SERIES DATA
+import statsmodels.api as sm
+sunspot_dataset = sm.datasets.sunspots.load_pandas().data
+
+# Remove the first 2 data points for the purpose of the lecture
+sunspot_dataset = sunspot_dataset.iloc[2:]
+
+# Organize the data format
+sunspot_dataset.index = pd.to_datetime(sunspot_dataset["YEAR"].astype(int), format='%Y')
+sunspot_dataset = sunspot_dataset["SUNACTIVITY"]
+
+plt.figure(figsize=(10, 5))
+sunspot_dataset.plot()
+plt.grid(True, linestyle='--', alpha=0.5)
+plt.show()
+
+# Import the plot_acf function to calculate and plot autocorrelation
+from statsmodels.graphics.tsaplots import plot_acf
+
+fig, ax = plt.subplots(figsize=(10, 7))
+# Plot autocorrelation. By specifying lags=11, autocorrelation for 11 years (1 cycle) is calculated
+plot_acf(sunspot_dataset, lags=11, ax=ax)
+plt.xlabel('Lag')
+plt.show()
+
+# the pattern shows a damped oscillation, where correlations weaken as the lag increases but remain significant at certain intervals.
+# This indicates that the series exhibits strong autocorrelation over multiple years.
+
+fig, ax = plt.subplots(figsize=(10, 7))
+# This time, set lags = 50
+plot_acf(sunspot_dataset, lags=50, ax=ax)
+plt.xlabel('Lag')
+plt.show()
+# Here, the correlogram shows a wavy pattern.
+# the correlation coefficients get high and low every 10 or 11 years - periodic patterns
+
+
+# For independent sequence: the correlogram hits almost zero whenever lag >=1. This indicates the trials are independent, and maybe you have to consider if you can apply time series models, which usually assumes autocorrelative nature in the data.
+import numpy as np
+dice_data = np.random.randint(1,7,5000)
+
+fig, ax = plt.subplots(figsize=(10, 7))
+plot_acf(dice_data, ax=ax)
+plt.xlabel('Lag')
+plt.show()
+
+# STL Decomposition (Seasonal and Trend decomposition using Loess)
+# method for decomposing time series data into trend, seasonal, and residual components.
+# key parameters:
+# period: the length of the seasonal cycle (must be set manually)
+# seasonal: smoothing window size for the seasonal component
+# trend: smoothing window size for the trend component
+# robust: whether to use robust fitting (helps handle outliers) We first instantiate it and then call fit() method. By doing so, it returns a result object with attributes such as, trend, seasonal, resid
+
+from statsmodels.tsa.seasonal import STL
+
+# STL decomposition
+stl = STL(sunspot_dataset, period=11)
+res = stl.fit()
+
+# Plotting each component
+
+fig, axes = plt.subplots(4,1,figsize=(10,8), sharex=True)
+# sharex = True: All subplots in the entire grid share the same x-axis. Ticks and labels are automatically hidden on inner plots to reduce clutter. (default False)
+
+sunspot_dataset.plot(ax = axes[0])
+axes[0].grid(True, linestyle = '--', alpha=0.5)
+axes[0].set_title('Original Data')
+
+res.trend.plot(ax=axes[1])
+axes[1].grid(True, linestyle='--', alpha=0.5)
+axes[1].set_title('Trend')
+# trend component appears to fluctuate irregularly but there is a gradual increase
+
+res.seasonal.plot(ax=axes[2])
+axes[2].grid(True, linestyle='--', alpha=0.5)
+axes[2].set_title('Seasonal')
+# seasonality is clearly evident
+
+res.resid.plot(ax=axes[3])
+axes[3].grid(True, linestyle='--', alpha=0.5)
+axes[3].set_title('Residual')
+# represents prediction errors or noise and appears irregular in shape
+
+plt.tight_layout()
+plt.show()
+
+#-------------------------------------------------
