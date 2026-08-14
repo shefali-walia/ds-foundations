8b3936fe713d531550a0e7754175887f86a98729
shefali-walia <shefali.walia.04@gmail.com>
2026-08-14 14:05:11 +0530

    Week10-time-series: practice.py - Shifting, pct_change, rolling mean and std. dev.

--- week10-time-series/practice.py
+++ week10-time-series/practice.py
@@ -61,3 +61,38 @@ print(df_no2.shape)                      # (1930,1)
 print(df_no2.resample('M').last().shape) # (67,1) => 67 Months of data
 
 #-------------------------------------------------
+# SHIFTING AND PERCENT CHANGE
+
+no2_ratio = df_no2.resample('M').last() / df_no2.resample('M').last().shift(1)
+print(no2_ratio.head(10))
+
+no2_ratio_pct = df_no2.pct_change(periods=1, fill_method=None, freq=None)
+print(no2_ratio_pct.head(10))
+
+#-------------------------------------------------
+# MOVING AVG. AND STD. DEV.
+
+print(df_no2.rolling(7).mean().head(10))
+print(df_no2.rolling(7).std().head(10))
+# Shows NaN for the first 6 rows
+
+df_city = df_use[df_use['City'] == city]
+df_no2 = df_city[['NO2']].sort_index()
+full_range = pd.date_range(df_no2.index.min(), df_no2.index.max(), freq='D')
+df_no2 = df_no2.reindex(full_range)  # Gap in chart shows missing data
+
+plt.figure(figsize=(8,6))
+plt.plot(df_no2, color = 'Red', label='Daily NO2')
+plt.plot(df_no2.rolling(7).mean(), color = 'Black', label='7-day rolling avg')
+
+plt.title('Daily NO2 vs Weekly Rolling Average')
+plt.tight_layout()
+plt.grid(True)
+plt.legend()
+plt.show()
+
+print(df_no2.index.duplicated().sum())
+print(df_no2.index.is_monotonic_increasing)
+print(df_no2.loc['2017-06':'2017-09'])  # Missing data 
+
+#-------------------------------------------------
